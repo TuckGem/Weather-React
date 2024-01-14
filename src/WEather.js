ed2bc64efef5ab60156d814783267ea015a1894e
TuckGem <141772389+TuckGem@users.noreply.github.com>
2024-01-14 16:11:29 +0000

    Build continuesd

---
+++ src/WEather.js
@@ -0,0 +1,1 @@
+import "/.Weather.css";
---
+++ src/WeatherInfo.js
@@ -0,0 +1,35 @@
+import React from "react";
+import FormattedDates from "./FormattedDates";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDates date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/hr</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
